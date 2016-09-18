64d7edee892016b65cbd189c7a51032982381b42
Jani Turunen <jani@janisku7.eu>
2016-09-18 21:55:08 +0300

    safe drone landing

---
+++ FlightControlManagement/UserControls/LandingPad.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace FlightControlManagement.UserControls
+{
+    public sealed partial class LandingPad : UserControl
+    {
+        // Define a color brushes for the on screen representation of the LED.
+        private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
+        private SolidColorBrush whiteBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
+        private SolidColorBrush GreenBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
+        public LandingPad()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Safe Drone Landing
+more info in hackster.io 
+
+Drones land safe and others bevare 
